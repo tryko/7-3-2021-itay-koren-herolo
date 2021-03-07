6ef8d3b6769c700e4fff2382169073f548152903
itay <itay83ko@gmail.com>
2021-03-07 10:22:28 +0200

    seperated the view components

---
+++ src/views/List.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const List = () => {
+  const [isListTab, setIsListTab] = useState(true);
+
+  return <div>this is list</div>;
+};
+
+export default List;
---
+++ src/views/Recived.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Recived = () => {
+  return <div>this is recived</div>;
+};
+
+export default Recived;
